de6fcb2959055101a12529319f2e87a1070c4de8
Sungbin Lim <sunnybenlim@gmail.com>
2023-08-01 00:09:09 +0900

    refactor codes

--- tester.py
+++ tester.py
@@ -2,16 +2,14 @@ import torch
 from data import IonDataset
 from torch.utils.data import DataLoader
 from model import IonPredictor
-from util import make_valid_step, valid_loss_fn
+from trainer import make_valid_step, valid_loss_fn, device, data_dir, model
 
-device = 'cuda' if torch.cuda.is_available() else 'cpu'
-data_dir = './ion_data'
-
+device = device
+data_dir = data_dir
 batch_size=256
 test_data = IonDataset(data_dir, 'test')
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
-
-model = IonPredictor().to(device)
+model = model
 
 checkpoint = torch.load('model_checkpoint.pth')
 model.load_state_dict(checkpoint['model_state_dict'])
@@ -19,7 +17,7 @@ model.load_state_dict(checkpoint['model_state_dict'])
 test_step = make_valid_step(model, valid_loss_fn)
 
 with torch.no_grad():
-    test_MSE_loss=0
+    test_MSE_loss = 0
     cnt = 0
     for x_test_batch, y_test_batch_ion, y_test_batch_potential in test_dataloader:
         # 데이터 로더에서 받은 미니배치를 device 에 업로드
--- trainer.py
+++ trainer.py
@@ -4,7 +4,57 @@ import torch.optim as optim
 from data import IonDataset
 from torch.utils.data import DataLoader
 from model import IonPredictor
-from util import *
+import time
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        computation_time = end_time - start_time
+        print(f"Execution time of {func.__name__}: {computation_time} seconds")
+        return result
+    return wrapper
+
+def valid_loss_fn(predict, y):
+
+    ion_number_target, potential_target = y[0], y[1]
+    ion_number_predict, potential_predict = predict[0], predict[1]        
+    return loss_classifier(ion_number_predict, ion_number_target), loss_regression(potential_predict, potential_target)
+
+def train_loss_fn(predict, y):
+
+    ion_number_target, potential_target = y[0], y[1]
+    ion_number_predict, potential_predict = predict[0], predict[1]        
+    loss = loss_classifier(ion_number_predict, ion_number_target) + loss_regression(potential_predict, potential_target)
+    return loss
+
+def make_train_step(model, loss_fn, optimizer):
+    def train_step_fn(x, y):
+
+        model.train()
+        y_hat = model(x)
+        loss = loss_fn(y_hat, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        return loss.item()
+    return train_step_fn
+
+def make_valid_step(model, loss_fn):
+    
+    def valid_step_fn(x, y):
+        model.eval()
+        y_hat = model(x)        
+        model_prediction = torch.argmax(y_hat[0], axis=1)
+        target = torch.argmax(y[0], axis=1)
+        correct_cnt = torch.sum(model_prediction == target)
+
+        loss_classifier, loss_regressor = loss_fn(y_hat, y)
+        
+        return loss_classifier.item(), loss_regressor.item(), correct_cnt.item()
+    return valid_step_fn
 
 def train_model(train_loader, valid_loader, epochs=100, checkpoint=False, device='cpu'):
     train_losses = []
@@ -59,3 +109,21 @@ def train_model(train_loader, valid_loader, epochs=100, checkpoint=False, device
 
     return train_losses, valid_losses
 
+# Hyperparameter
+data_dir = './ion_data'
+batch_size = 512
+train_data = IonDataset(data_dir, 'train')
+valid_data = IonDataset(data_dir, 'valid')
+train_dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True, num_workers=8)
+valid_dataloader = DataLoader(valid_data, batch_size=batch_size, shuffle=False)
+lr = 0.1
+epochs = 30
+# Training setting
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+model = IonPredictor().to(device)
+optimizer = optim.SGD(model.parameters(), lr=lr)
+loss_classifier = nn.CrossEntropyLoss()
+loss_regression = nn.MSELoss()
+train_step = make_train_step(model, train_loss_fn, optimizer)
+valid_step = make_valid_step(model, valid_loss_fn)
+train_loss, valid_loss = train_model(train_dataloader, valid_dataloader, epochs=epochs, checkpoint=True, device=device)
