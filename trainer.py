b2f191c276bb10fa939a9f3685f614b971b7ba62
Sungbin Lim <sunnybenlim@gmail.com>
2023-07-31 13:22:57 +0900

    update valid eval code

--- trainer.py
+++ trainer.py
@@ -4,6 +4,7 @@ import torch.optim as optim
 from data import IonDataset
 from torch.utils.data import DataLoader
 from model import IonPredictor
+from util import *
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
@@ -14,31 +15,22 @@ def loss_fn(predict, y):
     loss = loss_classifier(ion_number_predict, ion_number_target) + loss_regression(potential_predict, potential_target)
     return loss
 
-def make_train_step(model, loss_fn, optimizer):
-    def train_step_fn(x, y):
-
-        model.train()
-        y_hat = model(x)
-        loss = loss_fn(y_hat, y)
-        loss.backward()
-        optimizer.step()
-        optimizer.zero_grad()
-
-        return loss.item()
-    return train_step_fn
-
-def train_model(train_loader, valid_loader, epochs=10):
+def train_model(train_loader, valid_loader, epochs=100):
+    train_losses = []
     valid_losses = []
     for epoch in range(epochs):
+        train_loss = 0
         for x_train_batch, y_train_batch_ion, y_train_batch_potential in train_loader:
             x_train_batch = x_train_batch.to(device)
             y_train_batch_ion = y_train_batch_ion.to(device)
             y_train_batch_potential = y_train_batch_potential.to(device)
             loss = train_step(x_train_batch, [y_train_batch_ion, y_train_batch_potential])
+            train_loss += loss
+        train_losses.append(train_loss / len(train_data))
             
         # Evaluate train loss
-        if epoch % 1 == 0:
-            print("train loss at {} epoch:{}".format(epoch, loss))
+        if epoch % 10 == 0:
+            print("train loss at {} epoch:{}".format(epoch, train_loss))
 
         # Evaluate valid loss
             with torch.no_grad():
@@ -47,9 +39,9 @@ def train_model(train_loader, valid_loader, epochs=10):
                     x_valid_batch = x_valid_batch.to(device)
                     y_valid_batch_ion = y_valid_batch_ion.to(device)
                     y_valid_batch_potential = y_valid_batch_potential.to(device)
-                    y_hat_val_ion, y_hat_val_potential = model(x_valid_batch)
-                    valid_loss += loss_fn([y_hat_val_ion, y_hat_val_potential], [y_valid_batch_ion, y_valid_batch_potential]).detach()
-                valid_loss = valid_loss / 10000
+                    eval_valid_loss = valid_step(x_valid_batch, )
+                    valid_loss += eval_valid_loss
+                valid_loss = valid_loss / len(valid_data)
                 print("valid loss at {} epoch:{}".format(epoch, valid_loss))
                 valid_losses.append(valid_loss)
 
@@ -66,6 +58,7 @@ loss_classifier = nn.CrossEntropyLoss()
 loss_regression = nn.MSELoss()
 
 train_step = make_train_step(model, loss_fn, optimizer)
+valid_step = make_valid_step(model, loss_fn)
 
 train_data = IonDataset(data_dir, 'train')
 valid_data = IonDataset(data_dir, 'valid')
---
+++ util.py
@@ -0,0 +1,33 @@
+import time
+import torch
+
+def timer(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        computation_time = end_time - start_time
+        print(f"Execution time of {func.__name__}: {computation_time} seconds")
+        return result
+    return wrapper
+
+def make_train_step(model, loss_fn, optimizer):
+    def train_step_fn(x, y):
+
+        model.train()
+        y_hat = model(x)
+        loss = loss_fn(y_hat, y)
+        loss.backward()
+        optimizer.step()
+        optimizer.zero_grad()
+
+        return loss.item()
+    return train_step_fn
+
+def make_valid_step(model, loss_fn):
+    def valid_step_fn(x, y):
+        model.eval()
+        y_hat = model(x)
+        loss = loss_fn(y_hat, y)
+        return loss.item()
+    return valid_step_fn
